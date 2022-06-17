1bbfb4fa2babaac00a430d18fae2e94037b98fa7
nikhildev8087 <nikhil.b@prometteur.in>
2022-06-17 18:47:07 +0530

    messages ui designed

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyDdV-KqJqVn4-Q0pm1iu3187F5uR1bR_8s",
+
+    authDomain: "linkedinmsg-85867.firebaseapp.com",
+
+    projectId: "linkedinmsg-85867",
+
+    storageBucket: "linkedinmsg-85867.appspot.com",
+
+    messagingSenderId: "29377870378",
+
+    appId: "1:29377870378:web:6a68ce6956ff8b57ba6153"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
+
